627888eabe428d27c7902dfa3a7f023d21d92472
MP <markpaololibunao@gmail.com>
2022-11-04 20:40:11 +0800

    Moves infra plugin registration to app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,12 +1,11 @@
 import { FastifyPluginAsync } from 'fastify'
 import { Config } from './config'
-import infra from './infra'
 
 export const app: FastifyPluginAsync<Config> = async (
 	fastify,
 	config
 ): Promise<void> => {
-	fastify.register(infra, config)
+	fastify.register(healthcheck, config)
 }
 
 export default app
