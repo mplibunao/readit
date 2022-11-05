4eeeefb5bef816bdbe4f2c0e5acee981d2c3b8c2
MP <markpaololibunao@gmail.com>
2022-11-05 11:34:20 +0800

    Fixes errors just to be sure

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsync } from 'fastify'
 import { Config } from './config'
+import healthcheck from './infra/healthcheck'
 
 export const app: FastifyPluginAsync<Config> = async (
 	fastify,
