732a9243b8403e8dc67c695691d7f68275b95848
MP <markpaololibunao@gmail.com>
2022-12-12 22:35:25 +0800

    Runs prettier on kysely generated types

--- apps/api/src/infra/pg/pg.generated.ts
+++ apps/api/src/infra/pg/pg.generated.ts
@@ -1,22 +1,26 @@
-import { ColumnType, RawBuilder } from "kysely";
+import { ColumnType, RawBuilder } from 'kysely'
 
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+	? ColumnType<S, I | undefined, U>
+	: ColumnType<T, T | undefined, T>
 
-export type Timestamp = ColumnType<Date, Date | string | RawBuilder, Date | string | RawBuilder>;
+export type Timestamp = ColumnType<
+	Date,
+	Date | string | RawBuilder,
+	Date | string | RawBuilder
+>
 
 export interface Users {
-  createdAt: Generated<Timestamp>;
-  deactivatedAt: Timestamp | null;
-  email: string;
-  firstName: string;
-  hashedPassword: string;
-  id: Generated<string>;
-  lastName: string;
-  updatedAt: Generated<Timestamp>;
+	createdAt: Generated<Timestamp>
+	deactivatedAt: Timestamp | null
+	email: string
+	firstName: string
+	hashedPassword: string
+	id: Generated<string>
+	lastName: string
+	updatedAt: Generated<Timestamp>
 }
 
 export interface DB {
-  users: Users;
+	users: Users
 }
