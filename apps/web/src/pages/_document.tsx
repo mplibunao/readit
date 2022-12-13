4dbcfd3efe45622087188f7a060960c74b6906ca
MP <markpaololibunao@gmail.com>
2022-12-14 07:42:31 +0800

    Creates favicon component

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,10 +1,13 @@
+import { Favicon } from '@/components/Favicon'
 import { SEO } from '@/components/SEO'
 import { Html, Head, Main, NextScript } from 'next/document'
+
 export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
 				<SEO />
+				<Favicon />
 				<meta
 					name='apple-mobile-web-app-status-bar-style'
 					content='black-translucent'
