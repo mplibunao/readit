dad35eccdd6e02b84cfd9a27d278fc0382207ac7
MP <markpaololibunao@gmail.com>
2022-12-14 11:05:37 +0800

    Adds turbo test command

--- apps/web/src/icons/processed/icon-list.js
+++ apps/web/src/icons/processed/icon-list.js
@@ -1,15 +1,15 @@
 /* auto generated list of available icons */
 module.exports = {
-  "exclamation-cicle": {
-    "filename": "exclamation-cicle.svg"
-  },
-  "eye-slash": {
-    "filename": "eye-slash.svg"
-  },
-  "eye": {
-    "filename": "eye.svg"
-  },
-  "reddit": {
-    "filename": "reddit.svg"
-  }
+	'exclamation-cicle': {
+		filename: 'exclamation-cicle.svg',
+	},
+	'eye-slash': {
+		filename: 'eye-slash.svg',
+	},
+	eye: {
+		filename: 'eye.svg',
+	},
+	reddit: {
+		filename: 'reddit.svg',
+	},
 }
