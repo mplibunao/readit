26f6fcac597e9d088d4c634ffdd4d7ed3f6dab27
MP <markpaololibunao@gmail.com>
2022-12-14 11:35:47 +0800

    - Creates react-testing-library helpers for rendering and a11y - Creates spinner component

---
+++ apps/web/src/components/Spinner/Spinner.test.tsx
@@ -0,0 +1,8 @@
+import { testA11y } from '@/helpers/test/accessibility'
+import { render } from '@testing-library/react'
+import { Spinner } from '.'
+
+test('Spinner renders correctly', async () => {
+	const { container } = render(<Spinner />)
+	await testA11y(container)
+})
