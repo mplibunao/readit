8ca71549657fa9d7d01c29447f4a76e3184cc765
MP <markpaololibunao@gmail.com>
2022-12-15 21:32:00 +0800

    Fixes lint error

--- apps/web/src/env/client.mjs
+++ apps/web/src/env/client.mjs
@@ -11,6 +11,8 @@ export const formatErrors = (
 		.map(([name, value]) => {
 			if (value && '_errors' in value)
 				return `${name}: ${value._errors.join(', ')}\n`
+
+			return null
 		})
 		.filter(Boolean)
 
