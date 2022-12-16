a647f223dd00584eaf2adae1c33389930a2ced13
MP <markpaololibunao@gmail.com>
2022-12-16 14:24:46 +0800

    Stops using env for geturl since only 1 can be valid at a time

--- apps/web/src/utils/url.ts
+++ apps/web/src/utils/url.ts
@@ -1,9 +1,7 @@
-import { env } from '@/env/server.mjs'
-
 function getUrl() {
-	if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
 
-	return `http://localhost:${env.PORT ?? 3000}`
+	return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
 export const url = getUrl()
