6009792cb1ae95cbbf2ac627633f7ecd55869094
MP <markpaololibunao@gmail.com>
2022-12-28 12:18:29 +0800

    Allows string type of PORT in web

--- apps/web/src/env/schema.mjs
+++ apps/web/src/env/schema.mjs
@@ -8,7 +8,7 @@ export const serverSchema = z.object({
 	//DATABASE_URL: z.string().url(),
 	NODE_ENV: z.enum(['development', 'test', 'production']),
 	VERCEL_URL: z.optional(z.string().url()),
-	PORT: z.optional(z.number(z.number().int().positive())),
+	PORT: z.optional(z.union([z.number().int().positive(), z.string()])),
 	API_URL: z.string(),
 	APP_NAME: z.string(),
 	VERCEL_ENV: z.enum(['development', 'preview', 'production']),
