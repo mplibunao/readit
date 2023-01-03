e3832ac65c5bbe54a0243f740e920b591eafa2fe
MP <markpaololibunao@gmail.com>
2023-01-03 09:50:04 +0800

    - Generates baseline migration

---
+++ apps/pg-manager/prisma/migrations/0_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "kysely_migration" (
+    "name" VARCHAR(255) NOT NULL,
+    "timestamp" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "kysely_migration_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "kysely_migration_lock" (
+    "id" VARCHAR(255) NOT NULL,
+    "is_locked" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "kysely_migration_lock_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "email" TEXT NOT NULL,
+    "hashed_password" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deactivated_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "idx_users_email" ON "users"("email");
+
