9d15cfdd9fa8e49cbc6ecfca084e8b1570a721f4
MP <markpaololibunao@gmail.com>
2023-01-13 22:32:14 +0800

    Fixes issue with url paths

--- apps/web/src/utils/url.ts
+++ apps/web/src/utils/url.ts
@@ -1,22 +1,19 @@
 import { isServer } from './ssr'
 
 function getNextUrl() {
-	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-	if (process.env.NEXT_PUBLIC_VERCEL_URL)
-		return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 	if (!isServer()) return '' // csr should be relative anyway
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
 
-	return `http://llocalhost:${process.env.PORT ?? 3000}`
+	return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
 export const nextBaseUrl = getNextUrl()
-console.log('nextBaseUrl', nextBaseUrl) // eslint-disable-line no-console
 
 export function getApiBaseUrl() {
 	if (process.env.NEXT_PUBLIC_API_URL) return process.env.NEXT_PUBLIC_API_URL
 	if (process.env.API_URL) return process.env.API_URL
 
-	return 'http://llocalhost:4000/api'
+	return 'http://localhost:4000/api'
 }
 
 export const trpcUrl = `${getApiBaseUrl()}/trpc`
