a8383be3db4bf4c74d3375e67711ce168e44ab68
MP <markpaololibunao@gmail.com>
2023-01-20 21:24:40 +0800

    Removes dangling tests

--- apps/api/src/infra/healthcheck/server.test.ts
+++ apps/api/src/infra/healthcheck/server.test.ts
@@ -41,37 +41,4 @@ describe('health check', () => {
 		expect(res.metrics.heapUsed).toBeTypeOf('number')
 		expect(res.metrics.eventLoopUtilized).toBeTypeOf('number')
 	})
-
-	test('should return ok for the db field if db is responsive', async () => {
-		const opts = {
-			version: '1.0.0',
-			maxHeapUsedBytes: 1_000_000_000,
-			maxRssBytes: 1_000_000_000,
-			maxEventLoopUtilization: 0.98,
-			maxEventLoopDelay: 1_000,
-			exposeStatusRoute: {
-				routeResponseSchemaOpts,
-				url: '/health/server',
-				routeOpts: {
-					logLevel: 'debug',
-				},
-			},
-		}
-
-		const fastify = await build({
-			config: {
-				underPressure: opts,
-			},
-		})
-
-		const response = await fastify.inject({
-			method: 'GET',
-			url: opts.exposeStatusRoute.url,
-		})
-
-		expect(response.statusCode).toEqual(200)
-
-		const res = response.json()
-		expect(res.db).toEqual('ok')
-	})
 })
