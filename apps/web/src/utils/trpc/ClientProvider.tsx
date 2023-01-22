ff4c935b95a8346a44514718c302d49be570acc7
MP <markpaololibunao@gmail.com>
2023-01-22 14:03:11 +0800

    Renames clientprovider to trpcclientprovider to differentiate from other providers

--- apps/web/src/utils/trpc/ClientProvider.tsx
+++ apps/web/src/utils/trpc/ClientProvider.tsx
@@ -4,7 +4,7 @@ import { clientTrpc, client } from '@/utils/trpc/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
 
-export function ClientProvider({ children }: { children: ReactNode }) {
+export function TrpcClientProvider({ children }: { children: ReactNode }) {
 	const [queryClient] = useState(() => new QueryClient())
 	const [trpcClient] = useState(() => clientTrpc)
 	return (
