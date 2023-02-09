1ce5c7ea87a030ed6e9fc5a8c5680620ee7599bb
MP <markpaololibunao@gmail.com>
2023-02-09 20:47:49 +0800

    Updates email template

--- apps/api/src/infra/mailer/components/login.tsx
+++ apps/api/src/infra/mailer/components/login.tsx
@@ -28,11 +28,11 @@ export function LoginEmail({
 				companyAddress={companyAddress}
 				productName={productName}
 			>
-				<EmailHeading>Your login code for {productName}</EmailHeading>
+				<EmailHeading>Your login link for {productName}</EmailHeading>
 				<EmailButton href={loginUrl}>Login to {productName}</EmailButton>
 				<Paragraph>
-					This link and code will only be valid for the next 5 minutes. If the
-					link doesn't work, you can copy and paste this URL into your browser:{' '}
+					This link will only be valid for the next 5 minutes. If the link
+					doesn't work, you can copy and paste this URL into your browser:{' '}
 					<EmailLink href={loginUrl}>{loginUrl}</EmailLink>
 				</Paragraph>
 				<Paragraph>
